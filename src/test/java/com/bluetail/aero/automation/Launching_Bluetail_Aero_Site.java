c7c7f0f1de3f2f4a732217377eedd334a7039fbc
unknown <teknotrait.ashok+1@gmail.com>
2021-02-18 11:09:02 +0530

    first commit

---
+++ src/test/java/com/bluetail/aero/automation/Launching_Bluetail_Aero_Site.java
@@ -0,0 +1,79 @@
+package com.bluetail.aero.automation;
+
+import org.testng.Reporter;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class Launching_Bluetail_Aero_Site extends TestBase {
+	public Launching_Bluetail_Aero_Site() {
+		super();
+	}
+
+	@BeforeClass
+	public void doSkip() {
+
+		initialize();
+
+	}
+
+	/**
+	 * @param testID
+	 * @param url
+	 * @throws InterruptedException
+	 */
+
+	@Test(dataProvider = "getTestData")
+	public void SmokeTestCase(String testID, String url) throws InterruptedException {
+
+		System.out.println("Starting Test:" + testID);
+		if (testID != "") {
+			launchBrowser();
+			Thread.sleep(2000);
+
+			// Open Page URL: https://refstagingapp.bluetail.aero/
+			Reporter.log("Opening the Zumic page URL : " + url);
+			driver.navigate().to(url);
+			System.out.println(" The Bluetail_Aero page URL is opened : " + url);
+			Thread.sleep(3000);
+
+			
+			
+			WebUIAutomation.setText("INPUT_Login_Email", "teknotrait.ashok@gmail.com");
+			WebUIAutomation.setText("INPUT_Login_Password", "Bluetail@123");
+			WebUIAutomation.clickObj("BTN_Login");
+			Thread.sleep(5000);
+			
+			
+			
+			
+			
+		}
+
+		else {
+			throw new SkipException("Skipping / Ignoring - Script not Ready for Execution ");
+
+		}
+	}
+
+	@AfterMethod
+	public void preparingToShutDown() {
+		try {
+
+			closeBrowser();
+
+		} catch (Exception e) {
+
+		}
+
+	}
+
+	@DataProvider(name = "getTestData")
+	public static Object[][] getData() {
+
+		return WebUIAutomation.getData("bluetail");
+
+	}
+}
